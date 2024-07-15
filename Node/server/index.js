b1fca4e5c959913c11b84db811748a8b46a5af70
Gaveen_Buddhika <buddhik-se20022@stu.kln.ac.lk>
2024-07-15 23:16:09 +0530

    Update index.js

--- Node/server/index.js
+++ Node/server/index.js
@@ -1,4 +1,44 @@
-const express = require("express")
-const bodyParser = require("body-parser")
-const mongoose = require("mongoose")
-const cors = require("cors")
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(bodyParser.json());
+
+const port = 4000;
+mongoose.connect("mongodb+srv://gaveen:pass123@cluster0.3mwbfgj.mongodb.net/mydatabase?retryWrites=true&w=majority&appName=Cluster0",
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+
+);
+
+
+const connection = mongoose.connection;
+
+connection.once("open", () => {
+
+    console.log("mongodb connectred");
+}
+
+
+
+);
+
+
+
+app.listen(port, () => {
+
+    console.log("server running in port" + port);
+}
+
+
+);
+
+
+
+
